04d7cb101758a4188887e7a56476b508959d5b15
cmkxak <gcm466@naver.com>
2022-10-04 14:30:36 +0900

    변수값을 활용하여 나머지를 구하도록 수정

--- src/CurrencyCnt.java
+++ src/CurrencyCnt.java
@@ -10,8 +10,8 @@ public class CurrencyCnt {
         //25000원을 각 화폐 단위로 나누었을 때 몫과 나머지를 출력하세요.
         //각 화폐 단위는 위와 같이 각각 변수로 선언 해주세요.
         System.out.printf("5만원권 %d장 나머지:%d\n", refund/curr50000, refund%curr50000);
-        System.out.printf("1만원권 %d장 나머지:%d\n", refund/curr10000, refund%10000);
-        System.out.printf("5천원권 %d장 나머지:%d\n", refund/curr5000, refund%5000);
+        System.out.printf("1만원권 %d장 나머지:%d\n", refund/curr10000, refund%curr10000);
+        System.out.printf("5천원권 %d장 나머지:%d\n", refund/curr5000, refund%curr5000);
 
     }
 }
