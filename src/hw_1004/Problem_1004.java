caf253d8471ae32adf2d2b2060e07f8a8fbc2034
cmkxak <gcm466@naver.com>
2022-10-04 18:20:04 +0900

    과제 : codeUp 1001-1010 기초 문제 풀이

---
+++ src/hw_1004/Problem_1004.java
@@ -0,0 +1,7 @@
+package hw_1004;
+
+public class Problem_1004 {
+    public static void main(String[] args) {
+        System.out.println("'Hello'");
+    }
+}
---
+++ src/hw_1004/Problem_1006.java
@@ -0,0 +1,7 @@
+package hw_1004;
+
+public class Problem_1006 {
+    public static void main(String[] args) {
+        System.out.println("\""+ "!@#$%^&*()" +"\"");
+    }
+}
