6b948cb8e9b55f2b6b2b703b0accca8d59354506
cmkxak <gcm466@naver.com>
2022-10-05 10:46:11 +0900

    Class 구조 변경 : 생성자 추가 및 메소드 매개변수 제거

--- src/lecture_1005/Calculator.java
+++ src/lecture_1005/Calculator.java
@@ -1,20 +1,27 @@
 package lecture_1005;
 
 public class Calculator {
+    private int a;
+    private int b;
 
-    public int plus(int a, int b){
+    public Calculator(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int plus(){
         return a+b;
     }
 
-    public int minus(int a, int b){
+    public int minus(){
         return a-b;
     }
 
-    public int multiple(int a, int b){
+    public int multiple(){
         return a*b;
     }
 
-    public float divide(int a, int b){
+    public float divide(){
         return (float) a/b;
     }
 }
--- src/lecture_1005/CalculatorMain.java
+++ src/lecture_1005/CalculatorMain.java
@@ -3,11 +3,11 @@ package lecture_1005;
 public class CalculatorMain {
 
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.plus(10,20));
-        System.out.println(calculator.minus(10,20));
-        System.out.println(calculator.multiple(10,20));
-        System.out.println(calculator.divide(10,20));
+        Calculator calculator = new Calculator(10,20);
+        System.out.println(calculator.plus());
+        System.out.println(calculator.minus());
+        System.out.println(calculator.multiple());
+        System.out.println(calculator.divide());
     }
 
 }
