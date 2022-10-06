b9258cb7593900b9c94d954656d91243945ec9a1
cmkxak <gcm466@naver.com>
2022-10-06 13:18:22 +0900

    알파벳 판별 여부 로직 isAlphabet() 메소드로 분리

--- src/lecutre_1006/collection/map/AlphabetExercise.java
+++ src/lecutre_1006/collection/map/AlphabetExercise.java
@@ -7,16 +7,24 @@ import java.util.Map;
 
 public class AlphabetExercise {
     public static void main(String[] args) {
-        String s = "1!@#$%%^!@#$%^weq123123asdfa";
+        String s = "1!@#$%%^!@#$%^weq123123asdfaz";
         Map<Character, Integer> map = new HashMap<>();
         int count = 0;
 
         for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
-            if((c >= 65 && c<=90) || (c>=97 && c<=121)){
+            if(isAlphabet(c)){
                 map.put(c, ++count);
             }
         }
         System.out.println("알파벳 개수 : " + count);
+    }
+
+    public static boolean isAlphabet(char c){
+        if((c >= 65 && c<=90) || (c>=97 && c<=122)){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
