e0b14f33bed700293c6660f26dedde18fb6fea4d
cmkxak <gcm466@naver.com>
2022-10-06 10:15:52 +0900

    RandomNumberGenerator 구현체를 활용해 0-10까지의 중복되지 않은 난수 출력하기

--- src/lecture_1005/numbergenerator/RandomNumberGenerator.java
+++ src/lecture_1005/numbergenerator/RandomNumberGenerator.java
@@ -3,6 +3,6 @@ package lecture_1005.numbergenerator;
 public class RandomNumberGenerator implements NumberGenerator{
     @Override
     public int generate(int num) {
-        return (int) (Math.random() * 10) * num;
+        return (int) (Math.random() * num);
     }
 }
---
+++ src/lecutre_1006/list/RandomNumbersWithoutDuplicated.java
@@ -0,0 +1,23 @@
+package lecutre_1006.list;
+
+import lecture_1005.numbergenerator.NumberGenerator;
+import lecture_1005.numbergenerator.RandomNumberGenerator;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RandomNumbersWithoutDuplicated {
+    public static void main(String[] args) {
+        NumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        Set<Integer> numberSet = new HashSet<>();
+
+        for(int i=0;i<50;i++){
+            int nums = randomNumberGenerator.generate(10);
+            numberSet.add(nums);
+        }
+
+        for (Integer num : numberSet) {
+            System.out.println(num);
+        }
+    }
+}
