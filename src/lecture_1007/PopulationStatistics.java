6a945a1caebeed03ed5d473580e2eaa79fd17184
cmkxak <gcm466@naver.com>
2022-10-07 14:46:43 +0900

    moveCnt() 메소드 추가 및 heatmap 파일 생성

--- src/lecture_1007/PopulationStatistics.java
+++ src/lecture_1007/PopulationStatistics.java
@@ -4,8 +4,7 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class PopulationStatistics {
     //한문자 읽기
@@ -51,6 +50,7 @@ public class PopulationStatistics {
         return line;
     }
 
+    //원하는 데이터만 parsing
     public PopulationMove parse(String data){
         String[] split = data.split(",");
         return new PopulationMove(split[0], split[1]);
@@ -83,14 +83,42 @@ public class PopulationStatistics {
         return populationMove.getFromSido() + "," + populationMove.getToSido() + "\n";
     }
 
+    public Map<String, Integer> getMoveCnt(List<PopulationMove> pml){
+        Map<String, Integer> moveCnt = new HashMap<>();
+
+        for( PopulationMove pm : pml){
+            String key = pm.getFromSido() + "," + pm.getToSido();
+            if(moveCnt.get(key) == null){
+                moveCnt.put(key, 1);
+            }
+            moveCnt.put(key, moveCnt.get(key)+1);
+        }
+        return moveCnt;
+    }
+
     public static void main(String[] args) throws IOException {
         String address = "C:\\2021_인구관련연간자료_20220927_66125.csv";
         String fromToAddress = "./from_to.txt";
 
         PopulationStatistics ps = new PopulationStatistics();
-
         List<PopulationMove> pml = ps.readByLine(fromToAddress);
 
+        Map<String, Integer> moveCntMap = ps.getMoveCnt(pml);
+
+        //heatmap 파일 생성 관련 로직
+        String targetFilename = "./for_heatmap.txt";
+        ps.createAFile(targetFilename);
+        List<String> cntResult = new ArrayList<>();
+        Set<String> keys = moveCntMap.keySet();
+        for (String key : keys) {
+            String[] split = key.split(",");
+            String s = String.format("[%s, %s, %d]\n", split[0], split[1], moveCntMap.get(key));
+            cntResult.add(s);
+        }
+        ps.write(cntResult, targetFilename);
+
+
+        //파일 생성
 //        List<String> strings = new ArrayList<>();
 //        for (PopulationMove populationMove : pml) {
 //            String fromTo = ps.fromToString(populationMove);
@@ -98,8 +126,8 @@ public class PopulationStatistics {
 //        }
 //        ps.write(strings,"./from_to.txt");
 
-        for (PopulationMove pm : pml) {
-            System.out.printf("전입: %s, 전출:%s \n", pm.getFromSido(), pm.getToSido());
-        }
+//        for (PopulationMove pm : pml) {
+//            System.out.printf("전입: %s, 전출:%s \n", pm.getFromSido(), pm.getToSido());
+//        }
     }
 }
