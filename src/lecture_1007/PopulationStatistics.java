091258b75a8a2ba7b6d4e946738e931f2b7e5144
cmkxak <gcm466@naver.com>
2022-10-07 16:14:47 +0900

    인구 이동을 차트로 나타내주기 위해 도시 별 인덱스를 고정값으로 지정해주는 heatmapIdx() 메소드 생성

--- src/lecture_1007/PopulationStatistics.java
+++ src/lecture_1007/PopulationStatistics.java
@@ -96,6 +96,30 @@ public class PopulationStatistics {
         return moveCnt;
     }
 
+    public Map<String, Integer> headMapIdx(){
+        Map<String, Integer> map = new HashMap<>();
+
+        map.put("11", 0);
+        map.put("26", 1);
+        map.put("27", 2);
+        map.put("28", 3);
+        map.put("29", 4);
+        map.put("30", 5);
+        map.put("31", 6);
+        map.put("36", 7);
+        map.put("41", 8);
+        map.put("42", 9);
+        map.put("43", 10);
+        map.put("44", 11);
+        map.put("45", 12);
+        map.put("46", 13);
+        map.put("47", 14);
+        map.put("48", 15);
+        map.put("50", 16);
+
+        return map;
+    }
+
     public static void main(String[] args) throws IOException {
         String address = "C:\\2021_인구관련연간자료_20220927_66125.csv";
         String fromToAddress = "./from_to.txt";
@@ -104,6 +128,7 @@ public class PopulationStatistics {
         List<PopulationMove> pml = ps.readByLine(fromToAddress);
 
         Map<String, Integer> moveCntMap = ps.getMoveCnt(pml);
+        Map<String, Integer> heatmapIdx = ps.headMapIdx();
 
         //heatmap 파일 생성 관련 로직
         String targetFilename = "./for_heatmap.txt";
@@ -111,8 +136,8 @@ public class PopulationStatistics {
         List<String> cntResult = new ArrayList<>();
         Set<String> keys = moveCntMap.keySet();
         for (String key : keys) {
-            String[] split = key.split(",");
-            String s = String.format("[%s, %s, %d]\n", split[0], split[1], moveCntMap.get(key));
+            String[] fromto = key.split(",");
+            String s = String.format("[%s, %s, %d],\n", heatmapIdx.get(fromto[0]), heatmapIdx.get(fromto[1]), moveCntMap.get(key));
             cntResult.add(s);
         }
         ps.write(cntResult, targetFilename);
