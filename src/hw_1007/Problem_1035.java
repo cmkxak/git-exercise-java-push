a4b8937293284d4ac0ba1c6816ef0bb71e221540
cmkxak <gcm466@naver.com>
2022-10-10 15:16:40 +0900

    codeUp 1031 - 1040 과제 풀이

---
+++ src/hw_1007/Problem_1035.java
@@ -0,0 +1,13 @@
+package hw_1007;
+
+import java.util.Scanner;
+
+public class Problem_1035 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        int hex = Integer.parseInt(str, 16);
+        System.out.printf("%o", hex);
+    }
+}
---
+++ src/hw_1007/Problem_1039.java
@@ -0,0 +1,10 @@
+package hw_1007;
+
+import java.util.Scanner;
+
+public class Problem_1039 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLong() + sc.nextLong());
+    }
+}
