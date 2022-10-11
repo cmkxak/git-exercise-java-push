17a7f6b2b0be72e56a36656f8febd9fe0286079d
cmkxak <gcm466@naver.com>
2022-10-11 18:09:41 +0900

    [codeUp] 1620 자릿수 더하기 풀이

---
+++ src/lecture/lecture_1011/CodeUp1620.java
@@ -0,0 +1,26 @@
+package lecture.lecture_1011;
+
+import java.util.Scanner;
+
+public class CodeUp1620 {
+    public static void main(String[] args) {
+        CodeUp1620 c = new CodeUp1620();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        c.solution(n);
+    }
+
+    public void solution(int n){
+        int result = 0;
+
+        while(n > 0) {
+            result += n % 10;
+            n = n / 10;
+        }
+            if (result < 10){
+                System.out.println(result);
+            }else{
+                solution(result);
+            }
+        }
+}
