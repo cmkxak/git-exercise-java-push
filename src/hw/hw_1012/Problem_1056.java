dfbd982ebbe2d0b4a039b2ae7c05405375142994
cmkxak <gcm466@naver.com>
2022-10-13 00:04:42 +0900

    [codeUp] 1051 - 1060 과제 풀이

---
+++ src/hw/hw_1012/Problem_1056.java
@@ -0,0 +1,16 @@
+package hw.hw_1012;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem_1056 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String[] str = s.split(" ");
+
+        System.out.println(Integer.parseInt(str[0]) != Integer.parseInt(str[1]) ? 1 : 0);
+
+    }
+}
