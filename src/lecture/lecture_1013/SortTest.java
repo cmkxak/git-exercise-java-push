85aada139eeb1933c4391ca690e984a8a8d14b73
cmkxak <gcm466@naver.com>
2022-10-13 10:27:58 +0900

    선택정렬 x, 2중 for문을 돌면서, 작은 값을 찾을 경우 바로 swap

--- src/lecture/lecture_1013/SortTest.java
+++ src/lecture/lecture_1013/SortTest.java
@@ -2,8 +2,8 @@ package lecture.lecture_1013;
 
 import java.util.Arrays;
 
-public class SelectionSTest {
-    public int[] selctionSort(int [] arr){
+public class SortTest {
+    public int[] sort(int [] arr){
         int temp = 0;
 
         for(int i =0;i<arr.length-1;i++){
@@ -20,8 +20,8 @@ public class SelectionSTest {
 
     public static void main(String[] args) {
         int arr[] = new int[]{1,4,2,5,6,7,11,23,9,21};
-        SelectionSTest s = new SelectionSTest();
-        s.selctionSort(arr);
+        SortTest s = new SortTest();
+        s.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
