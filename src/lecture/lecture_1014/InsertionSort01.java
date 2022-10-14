942a84f087ac2510173bd8e64a567210b870efb1
cmkxak <gcm466@naver.com>
2022-10-14 09:37:09 +0900

    InsertionSort 기본 골격 설계

---
+++ src/lecture/lecture_1014/InsertionSort01.java
@@ -0,0 +1,19 @@
+package lecture.lecture_1014;
+
+import java.util.Arrays;
+
+public class InsertionSort01 {
+    public int[] insertionSort(int[] arr){
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = new int{8,5,6,2,4};
+        InsertionSort01 is = new InsertionSort01();
+
+        is.insertionSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
