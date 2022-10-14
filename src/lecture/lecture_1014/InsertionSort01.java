e021f2f4a217ddd4a30234882f562c96fad56929
cmkxak <gcm466@naver.com>
2022-10-14 09:53:16 +0900

    InsertionSort swap 로직 추가

--- src/lecture/lecture_1014/InsertionSort01.java
+++ src/lecture/lecture_1014/InsertionSort01.java
@@ -4,12 +4,19 @@ import java.util.Arrays;
 
 public class InsertionSort01 {
     public int[] insertionSort(int[] arr){
+        int i = 1;
+        int tmp;
 
+        if(arr[i] < arr[i-1]){
+            tmp = arr[i];
+            arr[i] = arr[i-1];
+            arr[i-1] = tmp;
+        }
         return arr;
     }
 
     public static void main(String[] args) {
-        int arr[] = new int{8,5,6,2,4};
+        int arr[] = new int[]{8,5,6,2,4};
         InsertionSort01 is = new InsertionSort01();
 
         is.insertionSort(arr);
