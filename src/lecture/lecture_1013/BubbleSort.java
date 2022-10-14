6fa3dd4375e73b294cd4727cde95fefc8f8fe9e7
cmkxak <gcm466@naver.com>
2022-10-14 09:37:48 +0900

    Bubble Sort 및 오름차순 정렬 알고리즘 추가

---
+++ src/lecture/lecture_1013/BubbleSort.java
@@ -0,0 +1,28 @@
+package lecture.lecture_1013;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    private int[] bubbleSort(int[] arr){
+        int tmp;
+
+        for(int i =0;i<arr.length;i++){
+            for(int j = 0 ; j < arr.length-i-1; j++){
+                if(arr[j] > arr[j+1]) {
+                    tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = new int[]{7,2,3,9,28,121,4,124,14,45,311,122,533,23,144,225,234,21,12,3,21,50,56};
+        BubbleSort bs = new BubbleSort();
+
+        bs.bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
--- src/lecture/lecture_1013/SortTest.java
+++ src/lecture/lecture_1013/SortTest.java
@@ -5,23 +5,21 @@ import java.util.Arrays;
 public class SortTest {
     public int[] sort(int [] arr){
         int temp = 0;
-
-        for(int i =0;i<arr.length-1;i++){
-            for (int j = i+1; j<arr.length; j++){
-                if (arr[i] > arr[j]){
-                    temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
-                }
+        int i = 0 ;
+        for (int j = i+1; j<arr.length; j++){
+            if (arr[i] > arr[j]){
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
             }
         }
         return arr;
     }
 
     public static void main(String[] args) {
-        int arr[] = new int[]{1,4,2,5,6,7,11,23,9,21};
+        int arr[] = new int[]{7,2,3,9,28,11};
         SortTest s = new SortTest();
-        s.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] result = s.sort(arr);
+        System.out.println(Arrays.toString(result));
     }
 }
