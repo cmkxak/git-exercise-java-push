442023fe6f0a5f6491515f9f412c0b8e1481339b
cmkxak <gcm466@naver.com>
2022-10-17 10:15:39 +0900

    피라미드 모양의 별 찍기

---
+++ src/lecture/lecture_1017/Pyramid.java
@@ -0,0 +1,20 @@
+package lecture.lecture_1017;
+
+public class Pyramid {
+    public void printPyramid(int n){
+        for(int i =0; i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0; k<i*2+1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Pyramid p = new Pyramid();
+        p.printPyramid(5);
+    }
+}
