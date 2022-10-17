f0d20f37acf8cdc0ce0bcb869bec154f9ed2e58e
cmkxak <gcm466@naver.com>
2022-10-17 09:25:35 +0900

    직각삼각형 모양의 별 찍기

--- src/lecture/lecture_1017/RightTriangle.java
+++ src/lecture/lecture_1017/RightTriangle.java
@@ -1,11 +1,18 @@
 package lecture.lecture_1017;
 
+import java.util.Scanner;
+
 public class RightTriangle {
 
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("* *");
-        System.out.println("* * *");
-        System.out.println("* * * *");
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("개수를 입력하세요 : ");
+        int n = sc.nextInt();
+        for(int i =0;i<n;i++){
+            for(int j =0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
