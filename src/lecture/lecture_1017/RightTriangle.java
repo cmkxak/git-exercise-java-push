ab6b4c669551504e222f1bdc576384251d56cbaa
cmkxak <gcm466@naver.com>
2022-10-17 09:28:18 +0900

    별찍기 로직 메소드로 분리

--- src/lecture/lecture_1017/RightTriangle.java
+++ src/lecture/lecture_1017/RightTriangle.java
@@ -3,16 +3,20 @@ package lecture.lecture_1017;
 import java.util.Scanner;
 
 public class RightTriangle {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.printf("개수를 입력하세요 : ");
-        int n = sc.nextInt();
+    public void getRightTriangle(int n){
         for(int i =0;i<n;i++){
             for(int j =0;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        RightTriangle rt = new RightTriangle();
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("개수를 입력하세요 : ");
+        int n = sc.nextInt();
+        rt.getRightTriangle(n);
     }
 }
