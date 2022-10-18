741537b646c48aa66ab4db1148399e58657faee2
cmkxak <gcm466@naver.com>
2022-10-18 09:22:18 +0900

    직사각형 별 찍기

--- src/lecture/lecture_1018/SquareStar.java
+++ src/lecture/lecture_1018/SquareStar.java
@@ -3,7 +3,13 @@ package lecture.lecture_1018;
 import java.util.Scanner;
 
 public class SquareStar {
-    public void getSquareStar(int x, int y){
+    public void printSquare(int n){
+        for (int i = 0; i < n; i++) {
+            System.out.println("*".repeat(n));
+        }
+    }
+
+    public void printRectangluar(int x, int y){
         for(int i=0;i<x;i++){
             System.out.println("*".repeat(y));
         }
@@ -14,6 +20,6 @@ public class SquareStar {
         SquareStar square = new SquareStar();
         int x = sc.nextInt();
         int y = sc.nextInt();
-        square.getSquareStar(x,y);
+        square.printRectangluar(x,y);
     }
 }
