97a628841cda052e802c2fcd5e8119c6283fb970
cmkxak <gcm466@naver.com>
2022-10-24 10:21:33 +0900

    프로그래머스 k번째 수 우선 순위 큐 문제 풀이 추가

--- src/main/java/com/likelion/lecture/lecture_1024/KthNumber.java
+++ src/main/java/com/likelion/lecture/lecture_1024/KthNumber.java
@@ -13,4 +13,35 @@ public class KthNumber {
             }
             return answer;
         }
+     public int getKthNum(int[] array, int[]commands){
+         int fromIdx = commands[0]-1;
+         int toIdx = commands[1];
+         int kthIdx = commands[2]-1;
+         int result = 0;
+
+         PriorityQueue<Integer> pq = new PriorityQueue<>();
+         for (int i = fromIdx; i < toIdx; i++) {
+             pq.add(array[i]);
+         }
+
+         for(int i=0;i<=kthIdx;i++){
+            result = pq.poll();
+         }
+         return result;
+     }
+
+    public int[] solutionByPriorityQueue(int[] arr, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int i = 0; i < commands.length; i++) {
+            answer[i] = getKthNum(arr, commands[i]);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        KthNumber kthNum = new KthNumber();
+        int[] arr = new int[]{1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        kthNum.solutionByPriorityQueue(arr, commands);
+    }
 }
