12bbfe05966ae4104b5779537324f3e8366b088e
cmkxak <gcm466@naver.com>
2022-10-31 10:39:33 +0900

    프로그래머스 모의고사 max와 같으면 append하는 로직 추가

--- src/main/java/com/likelion/lecture/lecture_1028/MockExam.java
+++ src/main/java/com/likelion/lecture/lecture_1028/MockExam.java
@@ -1,33 +1,51 @@
 package com.likelion.lecture.lecture_1028;
 
+import java.util.ArrayList;
+
 public class MockExam {
-        public int[] solution(int[] answers) {
-            int[] answer = {};
+    public int[] solution(int[] answers) {
+        ArrayList<Integer> answer = new ArrayList<>();
 
-            int []student1 = {1,2,3,4,5};
-            int []student2 = {2,1,2,3,2,4,2,5};
-            int []student3 = {3,3,1,1,2,2,4,4,5,5};
+        int student1[] = {1,2,3,4,5};
+        int student2[] = {2,1,2,3,2,4,2,5};
+        int student3[] = {3,3,1,1,2,2,4,4,5,5};
 
-            int []count = new int[3];
+        int count[] = new int[3];
 
-            //나머지 연산자를 활용하여 각 수포자의 정답을 담기
-            for(int i=0;i<answers.length;i++){
-                if (answers[i] == student1[i%5]){
-                    count[0]++;
-                }
-
-                if (answers[i] == student2[i%8]) {
-                    count[1]++;
-                }
-
-                if (answers[i] == student3[i%10]) {
-                    count[2]++;
-                }
+        //나머지 연산자를 활용하여 각 정답을 담아주기.
+        for(int i=0;i<answers.length;i++){
+            if (answers[i] == student1[i%5]){
+                count[0]++;
             }
 
+            if (answers[i] == student2[i%8]) {
+                count[1]++;
+            }
 
-            //가장 높은 점수를 받은 사람 -> 오름차순 정렬하기
-
-            return answer;
+            if (answers[i] == student3[i%10]) {
+                count[2]++;
+            }
         }
+        int maxScore = Math.max(count[0], Math.max(count[1], count[2]));
+        //배열로 최댓값이 같은 경우를 비교하려고 했지만, 높은 점수를 받은 사람이 한명인 경우
+        // 3개의 크기에서 나머지 배열의 자리는 자동으로 0으로 초기화 되기 때문에 list로 활용
+        for(int i=0;i<count.length;i++){
+            if (count[i] == maxScore){
+                answer.add(i+1);
+            }
+        }
+
+        int maxStudentList[] = new int[answer.size()];
+        int idx = 0;
+
+        for (int ans : answer){
+            maxStudentList[idx++] = ans;
+        }
+        return maxStudentList;
+    }
+
+    public static void main(String[] args) {
+        MockExam exam = new MockExam();
+        exam.solution(new int[]{1, 2, 3, 4, 5});
+    }
 }
