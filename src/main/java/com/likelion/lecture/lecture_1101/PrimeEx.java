ed1f990dfead9a0425bb78c02e66665b82daface
cmkxak <gcm466@naver.com>
2022-11-01 09:37:03 +0900

    소수 구하기 - 로직 오류 수정

--- src/main/java/com/likelion/lecture/lecture_1101/PrimeEx.java
+++ src/main/java/com/likelion/lecture/lecture_1101/PrimeEx.java
@@ -2,17 +2,17 @@ package com.likelion.lecture.lecture_1101;
 
 public class PrimeEx {
     public boolean isPrime(int num){
-        for(int i=1;i<=num;i++){
+        for(int i=2;i<num;i++){
             if (num % i == 0){
-                return true;
+                return false;
             }
         }
-        return false;
+        return true;
     }
 
     public static void main(String[] args) {
         PrimeEx prime = new PrimeEx();
-        int arr[] = {13, 17, 19, 23};
+        int arr[] = {13, 17, 19, 8};
         for (int n : arr) {
             if(prime.isPrime(n))
                 System.out.println(n + "은 소수 입니다.");
