62dae4cefb0280af4131e0030bbbdd0b24da5bef
cmkxak <gcm466@naver.com>
2022-11-03 09:25:13 +0900

    에라토스테네스의 체 구현 완성

--- src/main/java/com/likelion/lecture/lecture_1102/PrimeEx.java
+++ src/main/java/com/likelion/lecture/lecture_1102/PrimeEx.java
@@ -1,55 +1,33 @@
 package com.likelion.lecture.lecture_1102;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class PrimeEx {
-    private final int N = 50;
-    List<Integer> nums;
 
-    public PrimeEx() {
-        this.nums = new ArrayList<>(N);
-    }
+    public int solution(int n) {
+        int answer = 0;
+        boolean arr[] = new boolean[n + 1];
 
-    public void setNums(){
-        for(int i = 2; i< N; i++){
-            nums.add(i);
-        }
-    }
+        Arrays.fill(arr, true);
 
-    public void getNums(){
-        nums.forEach((item) -> {
-            System.out.println(item);
-        });
-    }
-
-    public boolean isPrime(int num){
-        for(int i = 2; i * i < num; i++){
-            if(num % i == 0) {
-                return false;
+        for(int i=2;i<=Math.sqrt(n); i++){ //2부터 n의 제곱근 까지 모든 수를 확인
+            if(arr[i]){ //i가 소수인 경우 (남은 수인 경우)
+                int j=2;
+                //i를 제외한 모든 i의 배수 지우기
+                while (i*j <=n){
+                    arr[i * j] = false;
+                    j+=1;
+                }
             }
         }
-        return true;
-    }
-
-    public int solution(int num){
-        setNums();
-        for(int i = 2; i*i <= N; i++){
-            for (int j=0;j<nums.size();j++){
-                if(nums.get(j) % i == 0 && nums.get(j) > i)
-                    nums.remove(j);
-            }
+        for(int i= 2;i<=n;i++){
+            if(arr[i]) answer+=1;
         }
-        getNums();
-        return nums.size();
+        return answer;
     }
 
     public static void main(String[] args) {
         PrimeEx prime = new PrimeEx();
-        prime.solution(1000000);
+        prime.solution(50);
     }
-
-
-
 }
