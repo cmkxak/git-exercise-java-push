70baa10c9285acd6612e2d355aca18130b403b09
cmkxak <gcm466@naver.com>
2022-11-07 09:39:04 +0900

    프로그래머스 하샤드 수 로직 수정

--- src/main/java/com/likelion/lecture/lecture_1107/HarshadNumber.java
+++ src/main/java/com/likelion/lecture/lecture_1107/HarshadNumber.java
@@ -3,11 +3,12 @@ package com.likelion.lecture.lecture_1107;
 public class HarshadNumber {
     public boolean solution(int x) {
         int sum = 0;
+        int num = x;
 
         //1. 자릿수의 합 구하기
-        while (x > 0) {
-            sum += x % 10;
-            x /= 10;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
         }
 
         //2. 나누어 떨어지는지 check 하기
