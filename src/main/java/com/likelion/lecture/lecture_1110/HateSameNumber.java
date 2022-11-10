51aa207008141657dfa4a41d76571e24aef6e096
cmkxak <gcm466@naver.com>
2022-11-10 09:53:23 +0900

    [프로그래머스]같은 숫자는 싫어 Stack 구현 추가

--- src/main/java/com/likelion/lecture/lecture_1110/HateSameNumber.java
+++ src/main/java/com/likelion/lecture/lecture_1110/HateSameNumber.java
@@ -3,7 +3,7 @@ package com.likelion.lecture.lecture_1110;
 import java.util.*;
 
 public class HateSameNumber {
-    public int[] solution(int[] arr) {
+    public int[] solutionByList(int[] arr) {
         ArrayList<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < arr.length; i++) {
@@ -20,6 +20,23 @@ public class HateSameNumber {
 
         for (int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i);
+        }
+        return answer;
+    }
+
+    public int[] solutionByStack(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int n : arr) {
+            if (stack.isEmpty() || stack.peek() != n) {
+                stack.add(n);
+            }
+        }
+
+        int answer[] = new int[stack.size()];
+
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            answer[i] = stack.pop();
         }
         return answer;
     }
