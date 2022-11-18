bf3e2a10344fc3e63ab676dbc6dee9d236797d87
cmkxak <gcm466@naver.com>
2022-11-18 10:41:07 +0900

    [20221118] 행렬의 덧셈 : 프로그래머스 ArrayIndexOutofBoundsException

---
+++ src/main/java/com/likelion/lecture/week9/lecture_1118/SumOfProcession.java
@@ -0,0 +1,28 @@
+package com.likelion.lecture.week9.lecture_1118;
+
+//프로그래머스 불통과..
+
+public class SumOfProcession {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1.length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int arr1[][] = {{1, 2}, {2, 3}};
+        int arr2[][] = {{3, 4}, {5, 6}};
+
+        SumOfProcession sum = new SumOfProcession();
+        int[][] solution = sum.solution(arr1, arr2);
+        for (int i = 0; i < solution.length; i++) {
+            for (int j = 0; j<solution.length;j++){
+                System.out.println(solution[i][j]);
+            }
+        }
+    }
+}
