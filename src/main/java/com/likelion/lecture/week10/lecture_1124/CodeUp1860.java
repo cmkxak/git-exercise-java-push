3e1551494518023c9bbeb3f99ab51df9f7910975
cmkxak <gcm466@naver.com>
2022-11-24 09:43:28 +0900

    [20221124] (재귀) 수 삼각형 출력하기

---
+++ src/main/java/com/likelion/lecture/week10/lecture_1124/CodeUp1860.java
@@ -0,0 +1,22 @@
+package com.likelion.lecture.week10.lecture_1124;
+
+public class CodeUp1860 {
+    public void recursivePrintNum(int n){
+        if (n <= 0) return;
+        recursivePrintNum(n-1);
+        printNum(n);
+        System.out.println();
+    }
+
+    private void printNum(int n) {
+        if (n <= 0) return;
+        printNum(n-1);
+        System.out.print(n + " ");;
+    }
+
+
+    public static void main(String[] args) {
+        CodeUp1860 c = new CodeUp1860();
+        c.recursivePrintNum(5);
+    }
+}
