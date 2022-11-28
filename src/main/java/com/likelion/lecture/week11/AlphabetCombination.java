df01939a929d4f5526687a71662b6dc11695a822
cmkxak <gcm466@naver.com>
2022-11-28 09:47:07 +0900

    [20221128] (재귀) N개의 칸에 A-Z로 출력하기

--- src/main/java/com/likelion/lecture/week11/AlphabetCombination.java
+++ src/main/java/com/likelion/lecture/week11/AlphabetCombination.java
@@ -1,14 +1,32 @@
 package com.likelion.lecture.week11;
 
-public class AlphabetCombination {
-    public static void main(String[] args) {
-        for(char i = 'A'; i<='Z'; i++){
-            System.out.println(i);
-        }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-        for(char i = 'A'; i<='Z'; i++){
-            for(char j = 'A'; j<='Z';j++)
-                System.out.printf("%s%s\n", i,j);
-        }
+public class AlphabetCombination {
+    private void printAlphabet(int size, char c) {
+        //종료 조건
+        if (size <= 0) return;
+
+        System.out.println(c);
+
+        if (c >= 'Z')
+            c = 'A';
+        else
+            c = (char) (c + 1);
+        size -= 1;
+
+        printAlphabet(size, c);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        AlphabetCombination ac = new AlphabetCombination();
+
+        int size = Integer.parseInt(br.readLine());
+        char c = 'A';
+
+        ac.printAlphabet(size,c);
     }
 }
