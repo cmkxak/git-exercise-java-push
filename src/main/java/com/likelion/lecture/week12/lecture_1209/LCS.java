4c54f13b05434e92a85d43aaf3d516326b889e35
cmkxak <gcm466@naver.com>
2022-12-09 11:03:36 +0900

    [LCS] dp 기록 로직

--- src/main/java/com/likelion/lecture/week12/lecture_1209/LCS.java
+++ src/main/java/com/likelion/lecture/week12/lecture_1209/LCS.java
@@ -5,11 +5,18 @@ public class LCS {
         String str1 = "ABCDCBA"; //열
         String str2 = "DCABDC"; // 행
 
-        int dp[][] = new int[str2.length()][str1.length()];
+        int dp[][] = new int[str2.length()+ 1][str1.length() + 1];
 
-        for (int i = 0; i < str2.length(); i++) {
-            for (int j=0; j<str1.length(); j++){
-                System.out.printf("i:%s j:%s\n", str2.charAt(i), str1.charAt(j));
+        for (int i = 1; i <= str2.length(); i++) {
+            for (int j=1; j<= str1.length(); j++){
+                System.out.printf("i:%s j:%s\n", str2.charAt(i-1), str1.charAt(j-1));
+                //두개가 같으면 대각에서 가지고 옵니다.
+                //대각선 dp[i-1][j-1]
+                if (str2.charAt(i-1) == str1.charAt(j-1)){
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
             }
         }
     }
